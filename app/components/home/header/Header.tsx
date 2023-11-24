405ca7a166c3485e19362083b270e3875bb0f264
Marcelino Saquende <102360616+msaquende18@users.noreply.github.com>
2023-11-24 21:22:36 +0100

    Tolggy

--- app/components/home/header/Header.tsx
+++ app/components/home/header/Header.tsx
@@ -61,7 +61,7 @@ const Header: FC<Props> = ({ activarItem, setOpen, open, route, setRoute }) => {
         toast.success("Bem vindo de volta!!!");
       } 
     }
-    if (data === null) {
+    else {
       setLogout(true);         
     }
   }, [data, usuario, socialAuth, isSuccess]);
